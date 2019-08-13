30dc49374a8f6c10c939bed89ee9a726c82e7873
mcmartin25 <45849014+mcmartin25@users.noreply.github.com>
2019-08-13 20:55:32 +0800

    Add Username value

--- MConsole/My Project/Settings.Designer.vb
+++ MConsole/My Project/Settings.Designer.vb
@@ -89,6 +89,18 @@ Namespace My
                 Me("ssLocationDisp") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property username() As String
+            Get
+                Return CType(Me("username"),String)
+            End Get
+            Set
+                Me("username") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
